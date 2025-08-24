125f9d3611b449a39e097c08a14a4797517bc603
Thuy trinh ne <21522719@gm.uit.edu.vn>
2025-08-24 11:05:42 +0700

    feature/final

--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,4 @@
-// src/database/db.js
+// // src/database/db.js
 import { Sequelize } from "sequelize";
 
 import dotenv from "dotenv";
@@ -11,8 +11,25 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mssql",
+    dialectOptions: {
+      options: {
+        encrypt: false,
+        enableArithAbort: true,
+        trustServerCertificate: true,
+      },
+    },
   }
 );
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Kết nối database thành công!");
+  })
+  .catch((error) => {
+    console.error("Kết nối database thất bại:", error);
+  });
 
 export default sequelize;
